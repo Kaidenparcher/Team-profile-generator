2cba05bb70331da5c76a19455f2191a7e5502e95
Kaiden Parcher <ktparcher@.com>
2023-04-24 10:30:49 -0700

    add in template for chapes

--- index.js
+++ index.js
@@ -1,2 +1,41 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const svgTemplates = require('./shape'); // Import SVG templates from another file
+
+// Prompt the user for input
+inquirer.prompt([
+    {
+      type: 'input',
+      name: 'text',
+      message: 'Enter up to three characters:',
+      validate: (input) => /^[a-zA-Z0-9]{1,3}$/.test(input) || 'Please enter 1-3 alphanumeric characters only',
+    },
+    {
+      type: 'input',
+      name: 'textColor',
+      message: 'Enter text color (keyword or hex code):',
+      validate: (input) => /^#?[0-9a-fA-F]{3,6}$|^(black|silver|gray|white|maroon|red|purple|fuchsia|green|lime|olive|yellow|navy|blue|teal|aqua)$/i.test(input) || 'Please enter a valid color keyword or hex code',
+    },
+    {
+      type: 'list',
+      name: 'shape',
+      message: 'Choose a shape:',
+      choices: ['circle', 'triangle', 'square'],
+    },
+    {
+      type: 'input',
+      name: 'shapeColor',
+      message: 'Enter shape color (keyword or hex code):',
+      validate: (input) => /^#?[0-9a-fA-F]{3,6}$|^(black|silver|gray|white|maroon|red|purple|fuchsia|green|lime|olive|yellow|navy|blue|teal|aqua)$/i.test(input) || 'Please enter a valid color keyword or hex code',
+    },
+  ])
+    .then((answers) => {
+      // Generate SVG using selected template and user input
+      const svg = svgTemplates[answers.shape](answers.text, answers.textColor, answers.shapeColor);
+  
+      // Save SVG as file
+      fs.writeFileSync('logo.svg', svg);
+  
+      // Print success message to console
+      console.log('Generated logo.svg');
+    });
---
+++ shape.js
@@ -0,0 +1,32 @@
+// svgTemplates.js
+
+// Circle template
+const circleTemplate = (text, textColor, shapeColor) => `
+  <svg width="300" height="200">
+    <circle cx="150" cy="100" r="50" fill="${shapeColor}" />
+    <text x="150" y="115" font-size="60" font-weight="bold" text-anchor="middle" fill="${textColor}">${text}</text>
+  </svg>
+`;
+
+// Triangle template
+const triangleTemplate = (text, textColor, shapeColor) => `
+  <svg width="300" height="200">
+    <polygon points="150,50 250,150 50,150" fill="${shapeColor}" />
+    <text x="150" y="135" font-size="60" font-weight="bold" text-anchor="middle" fill="${textColor}">${text}</text>
+  </svg>
+`;
+
+// Square template
+const squareTemplate = (text, textColor, shapeColor) => `
+  <svg width="300" height="200">
+    <rect x="50" y="50" width="200" height="100" fill="${shapeColor}" />
+    <text x="150" y="115" font-size="60" font-weight="bold" text-anchor="middle" fill="${textColor}">${text}</text>
+  </svg>
+`;
+
+// Export the templates as an object
+module.exports = {
+  circle: circleTemplate,
+  triangle: triangleTemplate,
+  square: squareTemplate,
+};
